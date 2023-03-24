271c926ea5c23bfc3360f5b20e053e125b6487f8
Kaung Myat Han <arkarhtethan@gmail.com>
2023-03-24 20:26:18 +0630

    react native phone ripple naimation finished

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Feather } from "@expo/vector-icons";
+import { MotiView } from 'moti';
+import { StyleSheet, View } from 'react-native';
+import 'react-native-gesture-handler';
+import 'react-native-reanimated';
+import { Easing } from 'react-native-reanimated';
 
-export default function App() {
+const _color = "#6E01EF";
+const _size = 100;
+
+export default function App () {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.dot, styles.center]}>
+        {[...Array(3).keys()].map(index => (
+          <MotiView
+            from={{ opacity: 0.7, scale: 1 }}
+            animate={{
+              opacity: 0,
+              scale: 4,
+            }}
+            transition={{
+              delay: index * 400,
+              type: 'timing',
+              duration: 3000,
+              loop: true,
+              repeatReverse: false,
+              easing: Easing.out(Easing.ease)
+            }}
+            key={index}
+            style={[
+              StyleSheet.absoluteFillObject, styles.dot
+            ]} />
+        ))}
+        <Feather name="phone-outgoing" size={32} color="#fff" />
+      </View>
     </View>
   );
 }
@@ -17,4 +45,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dot: {
+    width: _size,
+    height: _size,
+    borderRadius: _size,
+    backgroundColor: _color,
+  },
+  center: {
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
